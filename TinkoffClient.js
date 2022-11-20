3c22990df17c849f89285cc5a105bef07897e1a8
netsky-dev <arenda244@yandex.ru>
2022-11-21 02:13:27 +0300

    define jest

---
+++ TinkoffClient.js
@@ -0,0 +1,12 @@
+import Payment from './lib/payment/Payment.js';
+
+const TinkoffClient = {
+ Payment
+ //Request
+}
+
+export default TinkoffClient
+//let client = await TinkoffClient.Payment.finish_authorize({ PaymentId: 1, Card: { obj: 1 } })
+//console.log(client)
+//let client = new TinkoffClient.Request().request
+//console.log(new TinkoffClient.Request().request({ path: 1 }))
